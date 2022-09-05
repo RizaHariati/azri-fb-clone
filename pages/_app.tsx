7c007beab88ee97cf9eb8ce22a353ed9070eda6e
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2022-09-05 16:27:19 +0700

    adding SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,14 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store, wrapper } from "../app";
 import { Provider } from "react-redux";
 import Head from "next/head";
+import Script from "next/script";
 
 config.autoAddCss = false;
 const description =
   "A clone of Facebook using Next.JS, Typescript as the core of this web design with the help of Redux/toolkit, Redux/persist and Next Redux Wrapper. For styling I use FontAwesome Icons and Tailwind CSS";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>AzriClone</title>
@@ -26,6 +27,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-67SXRGBGG8`}
+      />
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];`}
+        {`function gtag(){dataLayer.push(arguments);}`}
+        {`gtag('js', new Date());`}
+        {`gtag('config','G-67SXRGBGG8');`}
+      </Script>
+
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Layout>
@@ -33,7 +45,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           </Layout>
         </PersistGate>
       </Provider>
-    </div>
+    </>
   );
 }
 
