123dfbbaeb04b383f4222be15ee09371cfb31ba1
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2022-09-05 11:25:06 +0700

    Adding SEO

--- next.config.js
+++ next.config.js
@@ -17,13 +17,12 @@ module.exports = {
     ],
   },
   env: {
-    FACEBOOK_CLIENT_ID: "553515166431945",
-    FACEBOOK_CLIENT_SECRET: "d6cbbe226e46b5b36c7e57302d2759b0",
+    FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+    FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
     NEXTAUTH_URL: "http://localhost:3000/",
     NEXTAUTH_URL2: "https://nextauthexample.vercel.app/",
-    NEXTAUTH_SECRET: "Thisismysecretwhatisyours",
-    KEYWORD_API: "615d134132c9c40bf2a39437",
-    GOOGLE_KEY:
-      "riza, hariati, rizahariati, website, portfolio, azricoding, facebook",
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    KEYWORD_API: process.env.KEYWORD_API,
+    GOOGLE_KEY: process.env.GOOGLE_KEY,
   },
 };
